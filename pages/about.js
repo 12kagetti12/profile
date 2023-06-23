ef2850d49530af4d8e63b0fec9ee0eef5479feec
satoshiKageyama <kage.satoshi12@gmail.com>
2023-06-23 13:37:15 +0900

    create header component

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Header from './header';
+
+export default function About() {
+  return (
+    <>
+      <Header/>
+      <h1>Hello world</h1>
+    </>
+  );
+}
---
+++ pages/header.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+
+export default function Header(){
+  return (
+    <>
+      <header id="header">
+        <div className="l-header_flex_inlineTop">
+          <div className="l-header_addMargin l-addMaxWidth_x_center_y_column l-x_alignSide_y_center">
+            <h1 className="c-header_logo"><a href="#area_top"><img class="c-header_logo l-x_center_y_center" src="/logo.svg" alt="ロゴ"/></a></h1>
+            <nav className="p-header_nav">
+              <ul className="c-header_nav_noStyle" id="scroll_nav">
+                <li className="c-header_nav_x_right c-mediaOnly"><a href="#area_top">Top</a></li>
+                <li className="c-header_nav_x_right"><a href="#area_profile">Profile</a></li>
+                <li className="c-header_nav_x_right"><a href="#area_favorite">Work</a></li>
+                <li className="c-header_nav_x_right"><a href="#">Contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <button className="c-media_navButton">
+          <span className="c-media_navButton_line"></span>
+          <span>menu</span>
+        </button>
+      </header>
+    </>
+  );
+}
