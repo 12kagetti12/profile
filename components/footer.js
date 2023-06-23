3d6e61196683a92f17e888738d9bfbed807d97b1
satoshiKageyama <kage.satoshi12@gmail.com>
2023-06-23 20:50:14 +0900

    create footer component

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Footer() {
+  return(
+    <>
+      <footer className="l-footer l-x_center_y_column">
+        <p className="p-footerStyle">&copy; Satoshi Kageyama</p>
+      </footer>
+    </>
+  );
+} 
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
-import Header from './header';
+import Header from '../components/header';
+import Footer from '@/components/footer';
 
 export default function About() {
   return (
     <>
       <Header/>
       <h1>Hello world</h1>
+      <Footer />
     </>
   );
 }
