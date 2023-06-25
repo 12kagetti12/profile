3e70034c060e3c369f68214985ddc7f2914ed151
satoshiKageyama <kage.satoshi12@gmail.com>
2023-06-25 17:05:51 +0900

    📝 add css module code

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
-import { Header } from '@/components/header';
-import { Footer } from '@/components/footer';
+import { Header } from '@/components/header'
+import { Footer } from '@/components/footer'
+import styles from '@/styles/Home.module.css'
 
 export default function About() {
   return (
