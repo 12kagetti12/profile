37cbea0b5de7b3b173e244e0fb96d905c63718f4
satoshiKageyama <kage.satoshi12@gmail.com>
2023-06-26 21:51:14 +0900

    💄 fix footer className

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,8 @@ import styles from './footer.module.css'
 export function Footer() {
   return(
     <>
-      <footer className={`${styles.L_Footer} ${styles.L_X_center_Y_column}`} >
-        <p className={styles.P_FooterStyle}>&copy; Satoshi Kageyama</p>
+      <footer className={`${styles.l_footer} ${styles.l_xCenter_yColumn}`} >
+        <p className={styles.p_footerStyle}>&copy; Satoshi Kageyama</p>
       </footer>
     </>
   );
