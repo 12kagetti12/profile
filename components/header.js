7977013c65ecd5b9dbece5284f32ec71d1a58b51
satoshiKageyama <kage.satoshi12@gmail.com>
2023-07-01 14:32:44 +0900

    ♻️ add link compornet

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
-import styles from './header.module.css'
+import styles from './header.module.css';
+import Link from 'next/link';
 
 export function Header(){
   return (
@@ -9,10 +10,20 @@ export function Header(){
             <h1 className={styles.c_logo}><a href="#area_top"><img className={`${styles.c_logo} ${styles.l_xCenter_yCenter}`} src="/logo.svg" alt="logo"/></a></h1>
             <nav className={styles.p_nav}>
               <ul className={styles.c_nav_noStyle} id="scroll_nav">
-                <li className={`${styles.c_nav_xRight} ${styles.c_mediaOnly}`}><a href="#area_top">Top</a></li>
-                <li className={styles.c_nav_xRight}><a href="#area_profile">Profile</a></li>
-                <li className={styles.c_nav_xRight}><a href="#area_favorite">Work</a></li>
-                <li className={styles.c_nav_xRight}><a href="#">Contact</a></li>
+                <li className={`${styles.c_nav_xRight} ${styles.c_mediaOnly}`}>
+                  <a href="#area_top">Top</a>
+                </li>
+                <li className={styles.c_nav_xRight}>
+                  <a href="#area_profile">Profile</a>
+                </li>
+                <li className={styles.c_nav_xRight}>
+                  <a href="#area_favorite">Work</a>
+                </li>
+                <li className={styles.c_nav_xRight}>
+                  <Link href="#">
+                    Contact
+                  </Link>
+                </li>
               </ul>
             </nav>
           </div>
