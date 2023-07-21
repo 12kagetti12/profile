cdbe1a8d53c88dbb431dc1cd1fbe629f5b25cd0d
satoshiKageyama <kage.satoshi12@gmail.com>
2023-07-21 16:52:42 +0900

    ⚰️ remove unnecessary declarations

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,8 +5,6 @@ import { Header } from "@/components/Header/Header";
 import { Footer } from "@/components/Footer/Footer";
 import { ContentCard } from "@/components/ContentCard/CotentCard";
 
-const inter = Inter({ subsets: ["latin"] })
-
 const WORK = [
   {
     id: 0,
