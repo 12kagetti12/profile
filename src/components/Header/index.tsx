529798e91d63c9331be6c8790995c0156c0e4a03
satoshiKageyama <kage.satoshi12@gmail.com>
2023-09-15 21:36:55 +0900

    💄 add icon files

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,99 +1,53 @@
+import React from "react";
 import Link from "next/link";
-import { useState } from "react";
+import Image from "next/image";
+import House_fill from "@/components/icons/House.svg";
+import Me_fill from "@/components/icons/Me_fill.svg";
+import Work from "@/components/icons/Work.svg";
+import Mail from "@/components/icons/Mail.svg";
 
-export function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const handleOpenClose = () => {
-    if (window.matchMedia("(max-width: 640px)").matches) {
-      setMenuOpen((prevmenuOpen) => !prevmenuOpen);
-    }
-  };
-
+const Header = () => {
   return (
-    <header>
-      <div className="fixed top-0 z-50 flex h-20 w-full items-center justify-between bg-white sm:justify-center">
-        <div className="flex h-fit max-w-screen-lg items-center justify-between  px-2 sm:w-full">
-          <h1 className="w-28">
+    <header className="mx-4 flex h-20 items-center">
+      <h1 className="w-28">
+        <Link href="/#areaTop" scroll={false}>
+          <img className="w-fit" src="/logo.svg" alt="logo" />
+        </Link>
+      </h1>
+      <nav className="fixed bottom-0 left-0 h-20 w-full  bg-white sm:static sm:flex sm:h-fit sm:justify-end">
+        <ul className="flex w-full justify-around" id="scroll_nav">
+          <li className="px-4 py-6 sm:hidden">
             <Link href="/#areaTop" scroll={false}>
-              <img className="w-fit" src="/logo.svg" alt="logo" />
+              <House_fill className="fill-current text-[#393E46]" />
             </Link>
-          </h1>
-          <nav className="absolute right-0 h-full w-40 font-sans text-xl sm:static sm:flex sm:h-fit sm:justify-end">
-            <ul
-              className={
-                menuOpen
-                  ? "absolute top-0 flex h-screen w-fit flex-col bg-white transition-transform duration-500 sm:flex-row sm:justify-end sm:bg-transparent"
-                  : "absolute top-0 flex h-screen w-fit -translate-y-full flex-col bg-white transition-transform duration-500 sm:static sm:h-fit sm:translate-y-0 sm:flex-row sm:justify-end sm:bg-transparent sm:transition-none"
-              }
-              id="scroll_nav"
+          </li>
+          <li className="px-4 py-6">
+            <Link
+              className="sm:hoverUnderLine01"
+              href="/#areaProfile"
+              scroll={false}
             >
-              <li className="px-4 py-6 sm:hidden">
-                <Link href="/#areaTop" scroll={false} onClick={handleOpenClose}>
-                  Top
-                </Link>
-              </li>
-              <li className="px-4 py-6">
-                <Link
-                  className="sm:hoverUnderLine01"
-                  href="/#areaProfile"
-                  scroll={false}
-                  onClick={handleOpenClose}
-                >
-                  Profile
-                </Link>
-              </li>
-              <li className="px-4 py-6">
-                <Link
-                  className="sm:hoverUnderLine01"
-                  href="/#areaWork"
-                  scroll={false}
-                  onClick={handleOpenClose}
-                >
-                  Work
-                </Link>
-              </li>
-              <li className="px-4 py-6">
-                <Link
-                  className="sm:hoverUnderLine01"
-                  href="/contact"
-                  onClick={handleOpenClose}
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <button
-          className="relative right-5 h-10 w-10 sm:hidden"
-          onClick={handleOpenClose}
-        >
-          <div className="h-10 w-10">
-            <span
-              className={
-                menuOpen
-                  ? "absolute h-1 w-10 origin-left translate-x-1.5 translate-y-0 rotate-45 transform rounded-sm bg-[#393e46] transition-transform duration-500"
-                  : "absolute h-1 w-10 origin-left rotate-0 transform rounded-sm bg-[#393e46] transition-transform duration-500"
-              }
-            ></span>
-            <span
-              className={
-                menuOpen
-                  ? "translation absolute top-3 h-1 w-0 rounded-sm bg-transparent duration-500"
-                  : "translation absolute top-3 h-1 w-10 rounded-sm bg-[#393e46] duration-500"
-              }
-            ></span>
-            <span
-              className={
-                menuOpen
-                  ? "absolute top-6 h-1 w-10 origin-left translate-x-1.5 translate-y-1 -rotate-45 transform rounded-sm bg-[#393e46] transition duration-500"
-                  : "absolute top-6 h-1 w-10 origin-left rotate-0 transform rounded-sm bg-[#393e46] transition duration-500"
-              }
-            ></span>
-          </div>
-          <span className="absolute -bottom-2 w-10 text-center">menu</span>
-        </button>
-      </div>
+              <Me_fill className=" fill-current  text-[#ADB2BA]" />
+            </Link>
+          </li>
+          <li className="px-4 py-6">
+            <Link
+              className="sm:hoverUnderLine01"
+              href="/#areaWork"
+              scroll={false}
+            >
+              <Work className="fill-current  text-[#ADB2BA]" />
+            </Link>
+          </li>
+          <li className="px-4 py-6">
+            <Link className="sm:hoverUnderLine01" href="/contact">
+              <Mail className="fill-current  text-[#ADB2BA]" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
-}
+};
+
+export default Header;
