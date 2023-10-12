4d6ea5cc34c49c1bb0bdb7d7676471fee0417062
satoshiKageyama <kage.satoshi12@gmail.com>
2023-10-12 20:02:43 +0900

    💄 Fix paper content layout

--- src/components/PaperContentCard/index.tsx
+++ src/components/PaperContentCard/index.tsx
@@ -4,9 +4,10 @@ type Props = {
   imgSrc: string;
   media: string;
   text: string;
+  style: string;
 };
 
-const PaperContentCard = ({ imgSrc, media, text }: Props) => {
+const PaperContentCard = ({ imgSrc, media, text, style }: Props) => {
   const { ref, inView } = useInView({
     rootMargin: "-30px",
     threshold: 0.3,
@@ -17,20 +18,21 @@ const PaperContentCard = ({ imgSrc, media, text }: Props) => {
   return (
     <li
       ref={ref}
-      className={`sm:drop-shadow-sm" flex flex-col items-center bg-blue-200 pb-6
-        ${inView ? "scrollFadeIn " : "scrollFadeInHidden "}`}
+      className={`flex flex-col items-start pb-6 sm:flex-1 sm:items-start sm:drop-shadow-sm
+        ${inView ? "scrollFadeIn " : "scrollFadeInHidden "} ${style}`}
     >
       <img
-        className="aspect-video w-full"
+        className="aspect-video w-full sm:w-1/2"
         src={imgSrc}
         alt={media + "Img"}
         width={640}
         height={424}
         sizes="(max-width:640px) 80vw,(max-width:1200px) 40vw,25vw"
-        // priority
       />
-      <div className="flex w-full flex-col">
-        <h2 className="sm:hoverUnderLine01 pb-0.5">{title}</h2>
+      <div className="w-fll flex flex-col sm:w-1/2 sm:pl-2">
+        <div className="sm:hoverUnderLine01 flex w-fit flex-col pb-0.5">
+          <h2>{title}</h2>
+        </div>
         <p>{text}</p>
       </div>
     </li>
