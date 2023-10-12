716372cae742fa4554ae19c2af00c3f76162199a
satoshiKageyama <kage.satoshi12@gmail.com>
2023-10-12 20:03:16 +0900

    💄 Fix footer padding

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="flex justify-center pb-24 pt-20 sm:pb-4">
+    <footer className="flex justify-center pb-24 pt-4 sm:pb-4">
       <p className="text-sm text-gray-500">&copy; Satoshi Kageyama</p>
     </footer>
   );
