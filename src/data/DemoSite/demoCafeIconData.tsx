2d8ad5f85dc145c9f06ad91dbcd6a3640d656084
satoshiKageyama <kage.satoshi12@gmail.com>
2024-01-04 18:02:27 +0900

    🗃️ Separate the database part

---
+++ src/data/DemoSite/demoCafeIconData.tsx
@@ -0,0 +1,69 @@
+import iconDemoCafeLogo96px from "public/DemoSite/iconDemoCafeLogo96px.svg";
+import iconDemoCafeHome48px from "public/DemoSite/iconDemoCafeHome48px.svg";
+import iconDemoCafeMenu48px from "public/DemoSite/iconDemoCafeMenu48px.svg";
+import iconDemoCafeStory48px from "public/DemoSite/iconDemoCafeStory48px.svg";
+import iconDemoCafeMap48px from "public/DemoSite/iconDemoCafeMap48px.svg";
+import iconDemoCafeInstagram48px from "public/DemoSite/iconDemoCafeInstagram48px.svg";
+import iconDemoCafeArrowUp48px from "public/DemoSite/iconDemoCafeArrowUp48px.svg";
+
+type Icon = {
+  id: number;
+  iconSvg: React.FC;
+  text: string;
+  hrefSection: string;
+  applicableSections: string[];
+};
+
+const navIcons: Icon[] = [
+  {
+    id: 0,
+    iconSvg: iconDemoCafeLogo96px,
+    text: "logo",
+    hrefSection: "/demoCafe",
+    applicableSections: ["headerLogo"],
+  },
+  {
+    id: 1,
+    iconSvg: iconDemoCafeHome48px,
+    text: "home",
+    hrefSection: "/demoCafe/#areaTop",
+    applicableSections: ["headerNav"],
+  },
+  {
+    id: 2,
+    iconSvg: iconDemoCafeMenu48px,
+    text: "menu",
+    hrefSection: "/demoCafe/#areaMenu",
+    applicableSections: ["headerNav"],
+  },
+  {
+    id: 3,
+    iconSvg: iconDemoCafeStory48px,
+    text: "story",
+    hrefSection: "/demoCafe/#areaStory",
+    applicableSections: ["headerNav"],
+  },
+  {
+    id: 4,
+    iconSvg: iconDemoCafeMap48px,
+    text: "map",
+    hrefSection: "/demoCafe/#areaMap",
+    applicableSections: ["headerNav"],
+  },
+  {
+    id: 5,
+    iconSvg: iconDemoCafeInstagram48px,
+    text: "Instagram",
+    hrefSection: "/demoCafe/#areaMap",
+    applicableSections: ["sectionMap"],
+  },
+  {
+    id: 6,
+    iconSvg: iconDemoCafeArrowUp48px,
+    text: "arrowUp",
+    hrefSection: "/demoCafe/#areaTop",
+    applicableSections: ["main"],
+  },
+];
+
+export default navIcons;
---
+++ src/data/DemoSite/demoCafeMenuData.tsx
@@ -0,0 +1,71 @@
+type Menu = {
+  id: number;
+  name: string;
+  price: number;
+  classification: string;
+};
+
+const menus: Menu[] = [
+  {
+    id: 0,
+    name: "xxxx_drink",
+    price: 0o0,
+    classification: "drink",
+  },
+  {
+    id: 1,
+    name: "xxxx_drink",
+    price: 0o0,
+    classification: "drink",
+  },
+  {
+    id: 2,
+    name: "xxxx_drink",
+    price: 0o0,
+    classification: "drink",
+  },
+  {
+    id: 3,
+    name: "xxxx_drink",
+    price: 0o0,
+    classification: "drink",
+  },
+  {
+    id: 4,
+    name: "xxxx_drink",
+    price: 0o0,
+    classification: "drink",
+  },
+  {
+    id: 5,
+    name: "xxxx_food",
+    price: 0o0,
+    classification: "food",
+  },
+  {
+    id: 6,
+    name: "xxxx_food",
+    price: 0o0,
+    classification: "food",
+  },
+  {
+    id: 7,
+    name: "xxxx_food",
+    price: 0o0,
+    classification: "food",
+  },
+  {
+    id: 8,
+    name: "xxxx_other",
+    price: 0o0,
+    classification: "other",
+  },
+  {
+    id: 9,
+    name: "xxxx_other",
+    price: 0o0,
+    classification: "other",
+  },
+];
+
+export default menus;
