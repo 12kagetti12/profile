3ef14bb2ad6e89ee7434141038af4ea033d94665
satoshiKageyama <kage.satoshi12@gmail.com>
2024-01-07 15:59:51 +0900

    🎨 Create custom tailwind css at H2 style

--- src/pages/demoCafe.tsx
+++ src/pages/demoCafe.tsx
@@ -45,53 +45,81 @@ export default function DemoCafe() {
           </nav>
         </div>
       </header>
-        <main>
-          <section id="mainVisual">
-            <div className="mx-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                className="h-[100vh] w-auto object-cover"
-                src="/DemoSite/imgDemoCafeMainVisual.jpg"
-                alt="mainVisual"
-                width="1440"
-                height="1920"
+      <main>
+        <section id="mainVisual">
+          <div className="mx-0">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className="h-[100vh] w-auto object-cover"
+              src="/DemoSite/imgDemoCafeMainVisual.jpg"
+              alt="mainVisual"
+              width="1440"
+              height="1920"
               loading="lazy"
-              />
+            />
             <div className="absolute bottom-[12vh] flex w-full flex-col items-end bg-[#FFCA99] bg-opacity-60 px-4 backdrop-blur-sm">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                className="fill-current drop-shadow-md"
-                  src="/DemoSite/iconDemoCafeLogo96px.svg"
-                  alt="logo"
-                  width={96}
-                  height={96}
-                />
-              <h1 className="relative -top-4 z-10 mx-2 h-8 text-4xl text-white drop-shadow-md">
-                  Demo Cafe
-                </h1>
-              </div>
-            </div>
-          </section>
-          <section id="areaMenu" className="my-20 flex flex-col items-center">
-            <div className="relative mb-4">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                src="/DemoSite/imgDemoCafeMenu.jpg"
-                alt="menuImg"
-                width="1280"
-                height="1920"
+                className="fill-current drop-shadow-md"
+                src="/DemoSite/iconDemoCafeLogo96px.svg"
+                alt="logo"
+                width={96}
+                height={96}
               />
-              <h1 className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-white">
-                Menu
+              <h1 className="relative -top-4 z-10 mx-2 h-8 text-4xl text-white drop-shadow-md">
+                Demo Cafe
               </h1>
             </div>
-            <MenuBlock menuClassification="drink" />
-            <MenuBlock menuClassification="food" />
-            <MenuBlock menuClassification="other" />
-          </section>
-          <section id="areaMap"></section>
-        </main>
-      </header>
+          </div>
+        </section>
+        <section id="areaMenu" className="my-20 flex flex-col items-center">
+          <div className="relative mb-4">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/DemoSite/imgDemoCafeMenu.jpg"
+              alt="menuImg"
+              width="1280"
+              height="1920"
+            />
+            <h1 className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-white">
+              Menu
+            </h1>
+          </div>
+          <MenuBlock menuClassification="drink" />
+          <MenuBlock menuClassification="food" />
+          <MenuBlock menuClassification="other" />
+        </section>
+        <section id="areaStory" className="my-20 flex flex-col items-center">
+          <div className="relative mb-4">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/DemoSite/imgDemoCafeStory.jpg"
+              alt="storyImg"
+              width="1920"
+              height="1280"
+              loading="lazy"
+            />
+            <h1 className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 capitalize text-white">
+              story
+            </h1>
+          </div>
+          <div className="m-4 flex flex-col items-center">
+            <h2 className="demoCafeStyleH2">coffee</h2>
+            <p className="text-justify indent-4 text-[#614D3A]">
+              テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります
+            </p>
+            <p className="mt-2 text-justify indent-4 text-[#614D3A]">
+              テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります
+            </p>
+          </div>
+          <button className="rounded-sm bg-[#FFCA99] p-2 align-middle text-white">
+            more
+          </button>
+        </section>
+        <section id="areaMap" className="my-20 flex flex-col items-center">
+          map
+        </section>
+      </main>
     </>
   );
 }
