373ea4887707a4c7deac2cc6e65540904f26aea5
satoshiKageyama <kage.satoshi12@gmail.com>
2024-01-18 20:35:44 +0900

    ✨ Create map view for map section

--- src/pages/demoCafe.tsx
+++ src/pages/demoCafe.tsx
@@ -1,8 +1,26 @@
 import Link from "next/link";
 import MenuBlock from "@/components/DemoSite/demoCafeMenuBlock";
 import navIcons from "@/data/DemoSite/demoCafeIconData";
+import Button from "@/components/DemoSite/demoCafeButton";
+import {
+  APIProvider,
+  InfoWindow,
+  Map,
+  Marker,
+  useMarkerRef,
+} from "@vis.gl/react-google-maps";
+import { useState } from "react";
+
+const mapAPIkey: string = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
+const mapID: string = process.env.NEXT_PUBLIC_MAP_ID;
 
 export default function DemoCafe() {
+  const position = { lat: 35.65720571170869, lng: 139.6677287383595 };
+  const [markerRef, marker] = useMarkerRef();
+  const [infoWindowShow, setInfoWindowShow] = useState(true);
+  const toggleInfoWindow = () =>
+    setInfoWindowShow((previousState) => !previousState);
+  const closeInfoWindow = () => setInfoWindowShow(false);
   return (
     <>
       <header id="areaTop">
@@ -80,6 +98,7 @@ export default function DemoCafe() {
               alt="menuImg"
               width="1280"
               height="1920"
+              loading="lazy"
             />
             <h1 className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-white">
               Menu
@@ -112,12 +131,79 @@ export default function DemoCafe() {
               テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります
             </p>
           </div>
-          <button className="rounded-full bg-[#FFCA99] px-8 py-1 align-middle text-white">
-            more
-          </button>
+          <Button onClick={() => console.log("Hello world")}>more</Button>
         </section>
         <section id="areaMap" className="my-20 flex flex-col items-center">
-          map
+          <div className="relative mb-4">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/DemoSite/imgDemoCafeMap.jpg"
+              alt="mapImg"
+              width="1920"
+              height="1280"
+              loading="lazy"
+            />
+            <h1 className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-white">
+              Map
+            </h1>
+          </div>
+          <div className="mb-4 h-[50vh] w-[80vw]">
+            <APIProvider apiKey={mapAPIkey}>
+              <Map
+                center={position}
+                zoom={14}
+                mapId={mapID}
+                fullscreenControl={false}
+                mapTypeControl={false}
+                streetViewControl={false}
+              >
+                <Marker
+                  ref={markerRef}
+                  position={position}
+                  onClick={toggleInfoWindow}
+                ></Marker>
+                {infoWindowShow && (
+                  <InfoWindow anchor={marker} onCloseClick={closeInfoWindow}>
+                    <div className="flex flex-col items-center">
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      <img
+                        src="/DemoSite/iconDemoCafeMenu48px.svg"
+                        alt="demoCafeLogo"
+                        loading="lazy"
+                      />
+                      <h2 className="align-text-top text-2xl text-[#FFCA99]">
+                        Demo Cafe
+                      </h2>
+                      <p>0:00am to 0:00pm</p>
+                    </div>
+                  </InfoWindow>
+                )}
+              </Map>
+            </APIProvider>
+          </div>
+          <address className="flex flex-col items-start">
+            <dl className="flex text-left">
+              <dt className="w-20 text-[#FFCA99]">address</dt>
+              <dd className="max-w-[50vw] break-words text-[#614D3A]">
+                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+              </dd>
+            </dl>
+            <dl className="flex text-left">
+              <dt className="w-20 text-[#FFCA99]">open</dt>
+              <dd className="text-[#614D3A]">0:00am to 0:00pm</dd>
+            </dl>
+            <dl className="flex text-left">
+              <dt className="w-20 text-[#FFCA99]">SNS</dt>
+              <dd>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src="/DemoSite/iconDemoCafeInstagram48px.png"
+                  alt="InstagramIcon"
+                  loading="lazy"
+                ></img>
+              </dd>
+            </dl>
+          </address>
         </section>
       </main>
     </>
