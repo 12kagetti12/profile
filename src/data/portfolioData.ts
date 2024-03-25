8cf88e02359215bf041e448495131a715880c59a
satoshiKageyama <kage.satoshi12@gmail.com>
2024-03-25 21:34:32 +0900

    🎨 Segmented data for work database

---
+++ src/data/portfolioData.ts
@@ -0,0 +1,69 @@
+import { JobProperties } from "@/types/portfolioTypes";
+
+const portfolioJobs: JobProperties[] = [
+  {
+    id: 0,
+    occupation: "DTP",
+    media: "電機連合NAVI",
+    imgSrc: "/paperContent_NAVI.jpg",
+    client: "電機連合",
+    text: "URL（2022.1.25時点）",
+    url: "https://www.jeiu.or.jp/report/navi/2021/12000821.html",
+  },
+  {
+    id: 1,
+    occupation: "DTP",
+    media: "Paper",
+    imgSrc: "/paperContent01.jpg",
+    client: "###",
+    text: "仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事仕事",
+    url: "",
+  },
+  {
+    id: 2,
+    occupation: "DTP",
+    media: "Paper",
+    imgSrc: "/paperContent02.jpg",
+    client: "###",
+    text: "仕事仕事仕事仕事仕事仕事",
+    url: "",
+  },
+  {
+    id: 3,
+    occupation: "DTP",
+    media: "Paper",
+    imgSrc: "/paperContent10.jpg",
+    client: "###",
+    text: "仕事名",
+    url: "",
+  },
+  {
+    id: 4,
+    occupation: "DTP",
+    media: "Paper",
+    imgSrc: "/paperContent09.jpg",
+    client: "###",
+    text: "仕事仕事仕事仕事仕事仕事",
+    url: "",
+  },
+  {
+    id: 5,
+    occupation: "DTP",
+    media: "Paper",
+    imgSrc: "/paperContent12.jpg",
+    client: "###",
+    text: "仕事名",
+    url: "",
+  },
+  {
+    id: 6,
+    occupation: "WEB",
+    media: "Demo Site Cafe",
+    imgSrc: "/webThumbnailDemoCafe_md.webp",
+    client: "Demo",
+    text: "店の雰囲気が伝わるサイトの依頼を受けたと想定して作成しました。店内の様子、メニュー、お店のコンセプトや地図の掲載を想定しています。",
+    url: "/demoCafe",
+  },
+];
+
+export default portfolioJobs;
